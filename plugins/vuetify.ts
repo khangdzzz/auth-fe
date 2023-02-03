7730f4d634fbcd08c1477d0d46f1685f8ce8bbf5
Phong Nguyen <n.d.phong@azoom.jp>
2023-02-03 12:15:30 +0900

    :+1: Add vuetify override and default options

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -11,6 +11,59 @@ export default defineNuxtPlugin((nuxtApp) => {
       sets: {
         mdi
       }
+    },
+    defaults: {
+      VButton: {
+        color: 'primary'
+      },
+      VCheckbox: {
+        hideDetails: 'auto',
+        color: 'primary'
+      },
+      VRadio: {
+        hideDetails: 'auto',
+        color: 'primary'
+      },
+      VRadioGroup: {
+        hideDetails: 'auto',
+        color: 'primary'
+      },
+      VSelect: {
+        hideDetails: 'auto',
+        color: 'primary',
+        density: 'compact',
+        variant: 'outlined'
+      },
+      VTextarea: {
+        hideDetails: 'auto',
+        color: 'primary',
+        variant: 'outlined'
+      },
+      VTextField: {
+        hideDetails: 'auto',
+        color: 'primary',
+        density: 'compact',
+        variant: 'outlined'
+      }
+    },
+    theme: {
+      defaultTheme: 'defaultTheme',
+      themes: {
+        defaultTheme: {
+          dark: false,
+          colors: {
+            primary: '#174E69',
+            background: '#f9f9f7',
+            surface: '#174E69',
+            error: '#CD2B21',
+            'on-primary': '#FFFFFF',
+            'on-background': '#212321',
+            border: '#CBCDCB',
+            text: '#212321',
+            'text-placeholder': '#8C8F8C'
+          }
+        }
+      }
     }
   })
 
